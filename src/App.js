9b87dd4feb5e1a9541d7677430af07f6ed3ee097
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2021-10-14 02:18:50 +0530

    clean header and app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import "./App.css";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <h1>Header Section</h1>
+      <h1 className="display-1">Header Section</h1>
     </header>
   );
 };
