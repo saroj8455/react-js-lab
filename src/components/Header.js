9f937320a4041e347c9dc5a79a6f1efaa0a8c6ef
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2021-10-19 00:41:00 +0530

    navbar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,47 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <h1 className="display-1">Header Section</h1>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="home">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="container m-auto">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="home">
+                  Home <span className="sr-only">(current)</span>
+                </a>
+              </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button
+                className="btn btn-outline-success my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
